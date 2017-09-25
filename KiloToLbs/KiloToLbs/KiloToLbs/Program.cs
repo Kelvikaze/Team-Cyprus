3950bc1d83a3c38623699e4eac6b2ddc1dfac4a0
thanksalot <housemk1@ferris.edu>
2017-09-25 12:15:45 -0400

    Created kilo to pound converter

---
+++ KiloToLbs/KiloToLbs/KiloToLbs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KiloToLbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an amount of kilos to be converted to pounds");
+
+            string input = Console.ReadLine();
+
+            int kilos = Convert.ToInt32(input);
+
+            int pounds = Convert.ToInt32(kilos * 2.2046226218);
+
+            Console.Clear();
+
+            Console.WriteLine(kilos + " kilos converted to pounds is approximately");
+
+            Console.WriteLine(pounds + " lbs");
+
+            Console.ReadKey();
+        }
+    }
+}
