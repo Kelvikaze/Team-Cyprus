35633ef7f7b898be3b688b72286da071235b8575
Kelvin <kelvin.houseman@yeticgi.com>
2017-11-01 21:36:40 -0400

    Added example for search. Added system for adding students

--- Projects/Database/Database/Database/Program.cs
+++ Projects/Database/Database/Database/Program.cs
@@ -12,6 +12,7 @@ namespace Database
     class Program
     {
         internal static List<student> students = new List<student>();
+        internal static student NewStudent = new student();
 
         internal static Dictionary<int, student> studentID = new Dictionary<int, student>();
         internal static Dictionary<string, student> studentFirstName = new Dictionary<string, student>();
@@ -29,27 +30,92 @@ namespace Database
         {
             LoadStudents();
 
-            student NewStudent = new student();
+            //newStudent();
 
+            SearchFirstName(Console.ReadLine());
+
+            //SaveStudents();
+
+            //LoadStudents();
+
+            Console.ReadLine();
+        }
+
+        static void SearchFirstName(string input)
+        {
+            List<student> output = new List<student>();
+
+            foreach (var item in studentFirstName.Keys)
+            {
+                if (item.ToLower().Contains(input))
+                {
+                    output.Add(studentFirstName[item]);
+                }
+            }
+
+            if (output.Count > 0)
+            {
+                Console.WriteLine("Found student(s) with matching criteria");
+
+                foreach (var item in output)
+                {
+                    Console.WriteLine(item.FirstName + " " + item.LastName);
+                }
+            }
+        }
+
+        static void newStudent()
+        {
+            Console.WriteLine("Input the desired student ID");
             NewStudent.ID = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Input the desired student first name");
             NewStudent.FirstName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Input the desired student last name");
             NewStudent.LastName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Input the desired course ID");
             NewStudent.CourseID = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Input the desired course number");
             NewStudent.CourseNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Input the desired course name");
             NewStudent.CourseName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Input the desired credits");
             NewStudent.Credit = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Input the desired semester");
             NewStudent.Semester = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Input the desired year");
             NewStudent.Year = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("Input the desired course type");
             NewStudent.CourseType = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Input the desired course grade");
             NewStudent.CourseGrade = Console.ReadLine();
 
             students.Add(NewStudent);
+            Console.Clear();
+            Console.WriteLine("Added New Student:");
+            Console.WriteLine(NewStudent.ID);
+            Console.WriteLine(NewStudent.FirstName);
+            Console.WriteLine(NewStudent.LastName);
+            Console.WriteLine(NewStudent.CourseID);
+            Console.WriteLine(NewStudent.CourseNumber);
+            Console.WriteLine(NewStudent.CourseName);
+            Console.WriteLine(NewStudent.Credit);
+            Console.WriteLine(NewStudent.Semester);
+            Console.WriteLine(NewStudent.Year);
+            Console.WriteLine(NewStudent.CourseType);
+            Console.WriteLine(NewStudent.CourseGrade);
 
-            SaveStudents();
-
-            LoadStudents();
-
-            Console.ReadLine();
+            Console.ReadKey();
+            Console.Clear();
         }
 
         static void ClearCurrentEntries()
@@ -72,7 +138,7 @@ namespace Database
         static void LoadStudents()
         {
             ClearCurrentEntries();
-
+            Console.Clear();
             try
             {
                 using (Stream stream = File.Open("data.bin", FileMode.Open))
@@ -81,19 +147,29 @@ namespace Database
 
                     var students2 = (List<student>)bin.Deserialize(stream);
                     foreach (student boi in students2)
-                    {
-                        Console.Clear();
+                    {                        
                         Console.WriteLine(boi.ID);
+                        studentID.Add(boi.ID, boi);
                         Console.WriteLine(boi.FirstName);
+                        studentFirstName.Add(boi.FirstName, boi);
                         Console.WriteLine(boi.LastName);
+                        studentLastName.Add(boi.LastName, boi);
                         Console.WriteLine(boi.CourseID);
+                        studentCourseID.Add(boi.CourseID, boi);
                         Console.WriteLine(boi.CourseNumber);
+                        studentCourseNumber.Add(boi.CourseNumber, boi);
                         Console.WriteLine(boi.CourseName);
+                        studentCourseName.Add(boi.CourseName, boi);
                         Console.WriteLine(boi.Credit);
+                        studentCredit.Add(boi.Credit, boi);
                         Console.WriteLine(boi.Semester);
+                        studentSemester.Add(boi.Semester, boi);
                         Console.WriteLine(boi.Year);
+                        studentYear.Add(boi.Year, boi);
                         Console.WriteLine(boi.CourseType);
+                        studentCourseType.Add(boi.CourseType, boi);
                         Console.WriteLine(boi.CourseGrade);
+                        studentCourseGrade.Add(boi.CourseGrade, boi);
                     }
                 }
             }
@@ -120,7 +196,7 @@ namespace Database
         }
 
         [Serializable]
-        public class student
+        internal class student
         {
             public int ID = 0;          
             public string FirstName = null;
